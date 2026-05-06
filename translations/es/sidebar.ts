5f2a22207b31fed58c5bb3fbd9834bce636b730a
Skymax <crespo.maximo@gmail.com>
2026-05-06 12:44:36 -0300

    update: for builders spanish translations

--- translations/es/sidebar.ts
+++ translations/es/sidebar.ts
@@ -62,6 +62,27 @@ const sidebars: SidebarsConfig = {
             "for-token-holders/legal-documents",
             "for-token-holders/faq"
         ]
+    },
+    {
+        "type": "category",
+        "label": "Para Builders",
+        "collapsible": true,
+        "collapsed": true,
+        "link": {
+            "type": "doc",
+            "id": "for-builders/start-here"
+        },
+        "items": [
+            "for-builders/roles-and-permissions",
+            "for-builders/protocol-parameters",
+            "for-builders/disputes",
+            "for-builders/reputation",
+            "for-builders/contract-references",
+            "for-builders/fiat-to-fiat-remittance",
+            "for-builders/global-currency-expansion",
+            "for-builders/rollout-sequence",
+            "for-builders/faq"
+        ]
     }
   ]
 };
